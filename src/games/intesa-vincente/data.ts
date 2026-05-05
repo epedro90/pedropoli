444dc99036e747c6eab56a709c616340da9bc70e
Enrico Pedroni <enrico.pedroni@oltresolutions.it>
2026-05-05 12:41:50 +0200

    Add extended question/word sets for all games

--- src/games/intesa-vincente/data.ts
+++ src/games/intesa-vincente/data.ts
@@ -1,19 +1,102 @@
 export const WORDS: string[] = [
-  'Mare', 'Montagna', 'Telefono', 'Pizza', 'Cinema', 'Computer', 'Scuola', 'Vacanza',
-  'Aeroporto', 'Biblioteca', 'Chitarra', 'Elefante', 'Farfalla', 'Giardino', 'Hotel',
-  'Isola', 'Jeans', 'Kiwi', 'Lampone', 'Maschera', 'Nuvola', 'Ombrello', 'Panda',
-  'Quadro', 'Ragno', 'Spiaggia', 'Treno', 'Uva', 'Violino', 'Zaino', 'Albero',
-  'Bicicletta', 'Candela', 'Dado', 'Elicottero', 'Fiore', 'Gelato', 'Hamburger',
-  'Igloo', 'Jungla', 'Koala', 'Limone', 'Mongolfiera', 'Nocciola', 'Orca', 'Pinguino',
-  'Quokka', 'Razzo', 'Stella', 'Tostapane', 'Unicorno', 'Vulcano', 'Waterfall',
-  'Xilofono', 'Yak', 'Zebra', 'Ananas', 'Baobab', 'Cappello', 'Delfino', 'Formaggio',
-  'Grotta', 'Hamburger', 'Iride', 'Judo', 'Karate', 'Lago', 'Marmellata', 'Notte',
-  'Orso', 'Parco', 'Quaderno', 'Radice', 'Sabbia', 'Tavola', 'Uragano', 'Vento',
-  'Zione', 'Arcobaleno', 'Bottiglietta', 'Castello', 'Deserto', 'Equilibrio', 'Fumetto',
-  'Ghiaccio', 'Hamburger', 'Insalata', 'Jamba', 'Kanguro', 'Lanterna', 'Musica',
-  'Neve', 'Orologio', 'Pallone', 'Rucola', 'Serpente', 'Torta', 'Ufficio', 'Viaggio',
-  'Abbraccio', 'Bandiera', 'Cucchiaio', 'Dinosauro', 'Equatore', 'Fontana', 'Globo',
-  'Hamburger', 'Invenzione', 'Janome', 'Karaoke', 'Labirinto', 'Molecola', 'Narciso',
+  // --- CALCIO ITALIANO ---
+  'Portiere', 'Rigore', 'Fuorigioco', 'Scudetto', 'Pallone', 'Corner', 'Fallo', 'Gol',
+  'Arbitro', 'Stadio', 'Tifoso', 'Allenatore', 'Traversa', 'Palo', 'Serie A', 'Coppa Italia',
+  'Dribbling', 'Assist', 'Centravanti', 'Terzino', 'Centrocampista', 'Capitano', 'Fascia',
+  'Offside', 'Cartellino', 'Rosso', 'Giallo', 'Calcio d\'angolo', 'Rimessa', 'Portare',
+  'Juventus', 'Milan', 'Inter', 'Roma', 'Napoli', 'Lazio', 'Fiorentina', 'Atalanta',
+  'Totti', 'Del Piero', 'Maldini', 'Buffon', 'Pirlo', 'Cannavaro', 'Baggio', 'Rivera',
+  'Scuderia', 'Curva', 'Derby', 'Ultras', 'Settore ospiti', 'Esultanza', 'Doppietta', 'Tripletta',
+
+  // --- CALCIO ESTERO ---
+  'Messi', 'Ronaldo', 'Mbappé', 'Haaland', 'Neymar', 'Benzema', 'Lewandowski', 'Salah',
+  'Real Madrid', 'Barcellona', 'Bayern Monaco', 'Manchester City', 'Liverpool', 'Arsenal',
+  'Chelsea', 'PSG', 'Atletico Madrid', 'Dortmund', 'Ajax', 'Porto',
+  'Premier League', 'Bundesliga', 'Liga', 'Champions League', 'Europa League', 'Mondiale',
+  'Europeo', 'Copa America', 'Pallone d\'Oro', 'Scarpa d\'Oro',
+  'Wembley', 'San Siro', 'Bernabeu', 'Camp Nou', 'Old Trafford', 'Maracana',
+
+  // --- SPORT ---
+  'Tennis', 'Nuoto', 'Ciclismo', 'Atletica', 'Boxe', 'Rugby', 'Pallavolo', 'Basket',
+  'Baseball', 'Golf', 'Sci', 'Snowboard', 'Pattinaggio', 'Scherma', 'Canottaggio',
+  'Equitazione', 'Tiro con l\'arco', 'Lotta', 'Judo', 'Karate', 'Taekwondo',
+  'Ping pong', 'Badminton', 'Pallamano', 'Hockey', 'Pallanuoto', 'Triathlon', 'Pentathlon',
+  'Olimpiadi', 'Podio', 'Medaglia', 'Oro', 'Argento', 'Bronzo', 'Cronometro',
+  'Maratona', 'Sprint', 'Salto in alto', 'Lancio del disco', 'Staffetta',
+  'Federer', 'Nadal', 'Djokovic', 'Sinner', 'Bolt', 'Phelps', 'Indurain', 'Pantani',
+  'Formula 1', 'MotoGP', 'Rally', 'Pit stop', 'Pole position', 'Giro veloce', 'Sorpasso',
+
+  // --- ANIMALI ---
+  'Giraffa', 'Gorilla', 'Cobra', 'Panda', 'Koala', 'Canguro', 'Leopardo', 'Ghepardo',
+  'Rinoceronte', 'Ippopotamo', 'Pinguino', 'Delfino', 'Squalo', 'Polpo', 'Medusa',
+  'Granchio', 'Tartaruga', 'Coccodrillo', 'Camaleonte', 'Fenicottero', 'Pavone',
+  'Gufo', 'Aquila', 'Corvo', 'Pappagallo', 'Tucano', 'Pellicano', 'Orca',
+  'Balena', 'Narvalo', 'Platipus', 'Axolotl', 'Piranha', 'Piranha', 'Anaconda',
+  'Licaone', 'Iene', 'Sciacallo', 'Mangusta', 'Procione', 'Castoro', 'Lontra',
+  'Volpe', 'Lupo', 'Orso', 'Alce', 'Bisonte', 'Zebra', 'Gnu', 'Antilope',
+  'Lemure', 'Capibara', 'Armadillo', 'Bradipo', 'Formichiere', 'Tapiro',
+
+  // --- MUSICA ---
+  'Chitarra', 'Pianoforte', 'Batteria', 'Violino', 'Sassofono', 'Tromba', 'Flauto',
+  'Microfono', 'Concerto', 'Album', 'Singolo', 'Discoteca', 'DJ', 'Rap', 'Rock',
+  'Jazz', 'Pop', 'Opera', 'Coro', 'Spartito', 'Amplificatore', 'Basso', 'Tastiera',
+  'Accordo', 'Ritornello', 'Strofa', 'Bridge', 'Ritmo', 'Melodia', 'Armonia',
+  'Freddie Mercury', 'Elvis Presley', 'Michael Jackson', 'Beatles', 'Rolling Stones',
+  'David Bowie', 'Madonna', 'Beyoncé', 'Eminem', 'Bob Dylan', 'Jimi Hendrix',
+  'Vasco Rossi', 'Ligabue', 'Zucchero', 'Laura Pausini', 'Tiziano Ferro', 'Eros Ramazzotti',
+  'Festival di Sanremo', 'Tour', 'Backstage', 'Bis', 'Ovazione',
+
+  // --- MACCHINE E MOTO ---
+  'Ferrari', 'Lamborghini', 'Porsche', 'BMW', 'Mercedes', 'Audi', 'Volkswagen',
+  'Toyota', 'Tesla', 'Maserati', 'Alfa Romeo', 'Fiat', 'Bugatti', 'McLaren', 'Aston Martin',
+  'Schumacher', 'Senna', 'Hamilton', 'Verstappen', 'Leclerc', 'Vettel',
+  'Ducati', 'Yamaha', 'Honda', 'Kawasaki', 'Harley Davidson', 'Aprilia', 'Suzuki', 'KTM',
+  'Valentino Rossi', 'Marquez', 'Quartararo', 'Bagnaia',
+  'Motore', 'Carburatore', 'Turbo', 'Frizione', 'Marcia', 'Sterzo', 'Cofano',
+  'Pneumatico', 'Cerchio', 'Freno', 'Scarico', 'Serbatoio', 'Cruscotto',
+
+  // --- GEOGRAFIA ---
+  'Himalaya', 'Amazzonia', 'Sahara', 'Antartide', 'Artico', 'Vesuvio', 'Etna',
+  'Nilo', 'Tamigi', 'Mississippi', 'Amazzoni', 'Yangtze', 'Gange',
+  'Adriatico', 'Mediterraneo', 'Atlantico', 'Pacifico', 'Indiano', 'Artico',
+  'Tokyo', 'New York', 'Londra', 'Parigi', 'Berlino', 'Sydney', 'Dubai', 'Mumbai',
+  'Rio de Janeiro', 'Buenos Aires', 'Città del Messico', 'Pechino', 'Mosca', 'Cairo',
+  'Alpi', 'Dolomiti', 'Appennini', 'Andes', 'Rockies', 'Kilimanjaro', 'Everest',
+  'Sicilia', 'Sardegna', 'Corsica', 'Islanda', 'Madagascar', 'Groenlandia',
+
+  // --- STORIA ---
+  'Gladiatore', 'Faraone', 'Vikingo', 'Cavaliere', 'Castello', 'Colosseo', 'Piramide',
+  'Pirati', 'Samurai', 'Ninja', 'Legionario', 'Centurione', 'Imperatore', 'Re', 'Regina',
+  'Napoleone', 'Alessandro Magno', 'Giulio Cesare', 'Cleopatra', 'Leonardo da Vinci',
+  'Colombo', 'Magellan', 'Copernico', 'Galileo', 'Newton', 'Darwin', 'Einstein',
+  'Rivoluzione Francese', 'Rivoluzione Industriale', 'Prima Guerra Mondiale', 'Seconda Guerra',
+  'Muro di Berlino', 'Sbarco sulla Luna', 'Rinascimento', 'Medioevo', 'Illuminismo',
+
+  // --- CIBO E CUCINA ---
+  'Lasagna', 'Carbonara', 'Risotto', 'Tiramisù', 'Cannolo', 'Gelato', 'Sushi',
+  'Hamburger', 'Tacos', 'Croissant', 'Baguette', 'Spaghetti', 'Ravioli', 'Pesto',
+  'Bruschetta', 'Arancino', 'Cannelloni', 'Gnocchi', 'Polenta', 'Ossobuco',
+  'Steak', 'Salmone', 'Tonno', 'Aragosta', 'Tartufo', 'Parmigiano', 'Mozzarella',
+  'Prosciutto', 'Salame', 'Mortadella', 'Caprese', 'Insalata', 'Minestrone',
+  'Espresso', 'Cappuccino', 'Macchiato', 'Latte', 'Tè', 'Mojito', 'Spritz',
+
+  // --- DIVERTIMENTO E GIOCHI ---
+  'Karaoke', 'Briscola', 'Scopa', 'Tressette', 'Monopoly', 'Risiko', 'Scarabeo',
+  'Poker', 'Blackjack', 'Dadi', 'Frisbee', 'Bowling', 'Biliardo', 'Freccette',
+  'Ping pong', 'Calciobalilla', 'Escape room', 'Paintball', 'Karting',
+  'Videogioco', 'Console', 'PlayStation', 'Xbox', 'Nintendo', 'Tetris', 'Minecraft',
+
+  // --- CINEMA E TV ---
+  'Tarantino', 'Spielberg', 'Kubrick', 'Fellini', 'Oscar', 'Cannes', 'Festival',
+  'Titanic', 'Avengers', 'Batman', 'Superman', 'Spiderman', 'Star Wars', 'Matrix',
+  'Hobbit', 'Harry Potter', 'James Bond', 'Indiana Jones', 'Jurassic Park',
+  'Netflix', 'Prime Video', 'Disney', 'Pixar', 'Marvel', 'DC',
+
+  // --- GENERICA DIVERTENTE ---
+  'Selfie', 'Influencer', 'Meme', 'Viral', 'Like', 'Follower', 'Story', 'Reel',
+  'Autogrill', 'Coda in autostrada', 'Spiaggia affollata', 'Ferragosto', 'Capodanno',
+  'Hangover', 'Brindisi', 'Compleanno', 'Sorpresa', 'Confetti', 'Coriandoli',
+  'Zanzara', 'Sonnellino', 'Russare', 'Dieta', 'Palestra', 'Divano',
 ]
 
 export function getShuffledWords(): string[] {
